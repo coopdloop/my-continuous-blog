fd7c7108a69fa64e89c35021025763eb6464ca40
coopdloop <coopdevsec@proton.me>
2025-01-09 12:19:10 -0600

    seo: remove dupes

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import metaMapPlugin from "vite-plugin-react-meta-map";
 
 export default defineConfig({
     plugins: [
         react(),
-        metaMapPlugin({
-            pageMetaMapFilePath: "./src/pageMetaMap.ts",
-            pageTemplateFilePath: "./src/PageTemplate.tsx",
-        }),
     ],
     resolve: {
         alias: {
