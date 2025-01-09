9a4544f6e6afe78de492da757018f25d46e21e6b
coopdloop <coopdevsec@proton.me>
2025-01-09 11:58:11 -0600

    seo: lets try and fix it

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -20,60 +20,31 @@ export const SEO: React.FC<SEOProps> = ({
     slug = '',
     author = 'Cooper Wallace',
     publishDate,
-    readingTime
 }) => {
     const siteUrl = 'https://www.devsec-cooper.codes';
-    const siteTitle = 'Cooper Wallace Blog';
-    const twitterHandle = '@BeenBashed';
+    const extendedDescription = `${description} - Read more about ${title} and other engineering insights on Cooper Wallace's technical blog, covering topics in software development, security, and cloud architecture.`;
 
-    return (
+
+   return (
         <Helmet>
             {/* Basic */}
-            <title>{`${title} | ${siteTitle}`}</title>
-            <meta name="description" content={description} />
-            <meta name="robots" content="index, follow" />
-            <meta property="og:locale" content="en_US" />
+            <title>{`${title} | Cooper Wallace Blog`}</title>
+            <meta name="description" content={extendedDescription} />
 
             {/* Open Graph */}
-            <meta property="og:site_name" content={siteTitle} />
             <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={`${siteUrl}${image}`} />
-            <meta property="og:url" content={`${siteUrl}${slug ? `/post/${slug}` : ''}`} />
+            <meta property="og:description" content={extendedDescription} />
+            <meta property="og:image" content={image} />
             <meta property="og:type" content={article ? 'article' : 'website'} />
+            <meta property="og:url" content={`${siteUrl}/post/${slug}`} />
+            <meta property="article:published_time" content={publishDate} />
+            <meta property="article:author" content={author} />
 
             {/* Twitter */}
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:creator" content={twitterHandle} />
             <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={`${siteUrl}${image}`} />
-
-            {/* Additional for blog posts */}
-            {article && (
-                <>
-                    <meta property="article:author" content={author} />
-                    {publishDate && <meta property="article:published_time" content={publishDate} />}
-                    {readingTime && <meta property="article:reading_time" content={readingTime} />}
-                    <link rel="canonical" href={`${siteUrl}/post/${slug}`} />
-                    <script type="application/ld+json">
-                        {JSON.stringify({
-                            "@context": "https://schema.org",
-                            "@type": "BlogPosting",
-                            "headline": title,
-                            "image": [`${siteUrl}${image}`],
-                            "url": `${siteUrl}/post/${slug}`,
-                            "description": description,
-                            "datePublished": publishDate,
-                            "timeRequired": readingTime,
-                            "author": {
-                                "@type": "Person",
-                                "name": author
-                            }
-                        })}
-                    </script>
-                </>
-            )}
+            <meta name="twitter:description" content={extendedDescription} />
+            <meta name="twitter:image" content={image} />
         </Helmet>
     );
 };
